1af5b8c1aab5455af541223938bb6a4b1b91ab08
riverbobby <51435795+riverbobby@users.noreply.github.com>
2021-08-30 14:33:06 -0700

    forgot to fill in EditAssessment View and ViewModel...

--- JustinTownleyMobile/ViewModels/EditAssessmentViewModel.cs
+++ JustinTownleyMobile/ViewModels/EditAssessmentViewModel.cs
@@ -1,10 +1,34 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading.Tasks;
+using JustinTownleyMobile.Services;
+using JustinTownleyMobile.Views;
+using System.Linq;
+using Xamarin.Forms;
 
 namespace JustinTownleyMobile.ViewModels
 {
-    class EditAssessmentViewModel
+    public class EditAssessmentViewModel
     {
+        public Course Course { get; set; }
+
+        public EditAssessmentViewModel()
+        {
+            if (DatabaseService.CurrentCourseID > 0)
+            {
+                Refresh();
+            }
+            else
+            {
+                Course = new Course();
+            }
+        }
+        async Task Refresh()
+        {
+            var course = await DatabaseService.GetCourse(DatabaseService.CurrentCourseID);
+            Course = course;
+        }
+
     }
 }
